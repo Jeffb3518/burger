9dec383137e544a5eacc3c822b797cc915f4f2f7
Jeffrey Bauer <Jeffrey@Jeffreys-Air.attlocal.net>
2017-01-20 16:35:25 -0500

    updates

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -2,6 +2,6 @@ var express = require("express");
 
 var router = express.Router();
 // Import the model (burger.js) to use its database functions.
-var cat = require("../models/burger.js");
+var burger = require("../models/burger.js");
 
 module.exports = router;
